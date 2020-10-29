df32bef303099739b6129c049730a57398800bbd
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-29 15:52:25 +0100

    Add delete-favorites function

--- src/components/FavoriteImage.js
+++ src/components/FavoriteImage.js
@@ -1,11 +1,27 @@
 import React from "react";
+import IconButton from "./IconButton";
+import "./FavoriteImageList.css";
 
 export default function FavoriteImage({ photoId }) {
+  let currentFavorites = JSON.parse(localStorage.getItem("favorites") || "[]");
   return (
-    <img
-      src={`https://source.unsplash.com/${photoId}`}
-      alt="Favorite"
-      className="favorite-image"
-    />
+    <div>
+      <img
+        src={`https://source.unsplash.com/${photoId}`}
+        alt="Favorite"
+        className="favorite-image"
+      />
+      <IconButton
+        onClick={() => {
+          currentFavorites = currentFavorites.filter(
+            (favorite) => favorite !== photoId
+          );
+          localStorage.setItem("favorites", JSON.stringify(currentFavorites));
+        }}
+        className="favorites__icon-button"
+      >
+        🗑
+      </IconButton>
+    </div>
   );
 }
